048fa9793c36f6017b4ddb42be0c5a3eea203748
Gautam Dayal <gautam.dayal@yahoo.com>
2024-06-04 23:53:24 -0400

    outer walls, debugged wall drawer function

--- src/Environment.cpp
+++ src/Environment.cpp
@@ -1,7 +1,21 @@
 #include "Environment.h"
 
 Environment::Environment() {
+    // Defining outer walls
+    // Change this to storing walls in a txt file or similar and then parsing to contstruct the environment
+    Eigen::Vector2d top_wall_start(50, 50);
+    Eigen::Vector2d top_wall_end(750, 50);
+    Eigen::Vector2d bottom_wall_start(50, 550);
+    Eigen::Vector2d bottom_wall_end(750, 550);
+    Eigen::Vector2d right_wall_start(750, 50);
+    Eigen::Vector2d right_wall_end(750, 550);
+    Eigen::Vector2d left_wall_start(50, 50);
+    Eigen::Vector2d left_wall_end(50, 550);
 
+    this->add_wall({top_wall_start, top_wall_end});
+    this->add_wall({bottom_wall_start, bottom_wall_end});
+    this->add_wall({right_wall_start, right_wall_end});
+    this->add_wall({left_wall_start, left_wall_end});
 }
 
 void Environment::add_wall(std::pair<Eigen::Vector2d, Eigen::Vector2d> wall_to_add) {
@@ -9,28 +23,24 @@ void Environment::add_wall(std::pair<Eigen::Vector2d, Eigen::Vector2d> wall_to_a
 }
 
 void Environment::draw_wall(std::pair<Eigen::Vector2d, Eigen::Vector2d> wall) {
-    // Private helper function to deal with raylib rectangle stuff
     Eigen::Vector2d start = wall.first;
     Eigen::Vector2d end = wall.second;
     int height;
     int width;
-    if (start[0] == end[0]) {
-        // handle case where x values identical
+    if (start[1] == end[1]) {
+        std::cout << "Wall is horizontal" << std::endl;
         height = 10;
-        std::cout << start[1] << " " << end[1] << std::endl;
-        width = std::abs(start[1] - end[1]);
+        std::cout << "Width of wall: " << std::abs(start[0] - end[0]) << std::endl;
+        width = std::abs(start[0] - end[0]);
     } else {
-        // handle case where y values identical
-        height = std::abs(start[0] - end[0]);
+        height = std::abs(start[1] - end[1]);
         width = 10;
     }
-    std::cout << "h=" << height << ", w=" << width << std::endl;
     DrawRectangle(start[0], start[1], width, height, (Color){0, 0, 255, 255});
 }
 
 void Environment::draw_environment() {
     for (std::pair<Eigen::Vector2d, Eigen::Vector2d> wall : walls) {
-        // Draw each wall as a raylib rectangle
         draw_wall(wall);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,11 @@
 #include "Environment.h"
 
 int main() {
-    Eigen::Vector2d wall_start(200, 100);
-    Eigen::Vector2d wall_end(300, 100);
-
-    std::cout << wall_end << std::endl;
-
     InitWindow(800, 600, "Winda");
     SetTargetFPS(60);
 
     // Create a render texture to use as the background instead of calling drawing functions each frame
     Environment bingus = Environment();
-    bingus.add_wall({wall_end, wall_start});
     
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -24,6 +18,10 @@ int main() {
         // DrawRectangle(wall_start[0], wall_start[1], 5, 200, (Color){0, 0, 255, 255});
         // DrawRectangle(wall_end[0], wall_end[1], 200, 5, (Color){0, 0, 255, 255});
             bingus.draw_environment();
+            DrawRectangle(10, 10, 20, 20, (Color){255, 0, 0, 255});
+            DrawRectangle(10, 500, 20, 20, (Color){0, 255, 0, 255});
+            DrawRectangle(500, 500, 20, 20, (Color){0, 0, 255, 255});
+            DrawRectangle(500, 10, 20, 20, (Color){255, 255, 0, 255});
         EndDrawing();
     }
         
