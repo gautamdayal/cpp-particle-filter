91e631b08e9de568fac5e8eaacbd553af08773d9
Gautam Dayal <gautam.dayal@yahoo.com>
2024-06-08 21:08:29 -0400

    Add from file function completed

--- src/Environment.cpp
+++ src/Environment.cpp
@@ -18,23 +18,36 @@ Environment::Environment() {
     this->add_wall({bottom_wall_start, bottom_wall_end});
     this->add_wall({right_wall_start, right_wall_end});
     this->add_wall({left_wall_start, left_wall_end});
+    this->add_walls_from_file("src/walls.csv");
 }
 
 void Environment::add_wall(std::pair<Eigen::Vector2d, Eigen::Vector2d> wall_to_add) {
     this->walls.push_back(wall_to_add);
 }
 
-void Environment::add_walls(std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> walls_to_add) {
+void Environment::add_walls(const std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>>& walls_to_add) {
     for (std::pair<Eigen::Vector2d, Eigen::Vector2d> wall : walls_to_add) {
         this->add_wall(wall);
     }
 }
 
-void Environment::add_walls_from_file(std::string filename) {
+void Environment::add_walls_from_file(const std::string& filename) {
     std::ifstream ifs(filename);
     std::string current_line;
     while (std::getline(ifs, current_line)) {
-        
+        std::istringstream ss(current_line);
+        std::string value;
+        std::vector<double> current_row_data;
+
+        while (std::getline(ss, value, ',')) {
+            current_row_data.push_back(std::stod(value));
+        }
+
+        if (current_row_data.size() == 4) {
+            Eigen::Vector2d current_start(current_row_data[0], current_row_data[1]);
+            Eigen::Vector2d current_end(current_row_data[2], current_row_data[3]);
+            walls.push_back({current_start, current_end});
+        }
     }
 }
 
